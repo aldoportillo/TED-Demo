ec7cd2af5aa21f35d82ac2feb483705759d217b0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-10 13:30:44 -0500

    Generate QR code

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,13 @@
 class Person < ApplicationRecord
-end
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+  
+    def qr_code
+      qrcode = RQRCode::QRCode.new(Rails.application.routes.url_helpers.person_url(self, host: 'localhost', port: 3000))
+      qrcode.as_svg(module_size: 6)
+    end
+  end
