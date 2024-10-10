2bdf17fdefc8ace8a5d3b988854d437259614fda
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-10 11:59:15 -0500

    create user table

---
+++ db/migrate/20241010165323_create_people.rb
@@ -0,0 +1,15 @@
+class CreatePeople < ActiveRecord::Migration[7.2]
+  def change
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :github_url
+      t.string :devto_url
+      t.string :linkedin_url
+      t.string :photo_url
+
+      t.timestamps
+    end
+  end
+end
