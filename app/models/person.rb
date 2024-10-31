e903fabe8c81ae28ea71a0ca84ca0216b66c8eaa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-31 13:44:28 -0500

    Fix QR Code Rendered

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,7 +7,7 @@ class Person < ApplicationRecord
     end
   
     def qr_code
-      qrcode = RQRCode::QRCode.new(Rails.application.routes.url_helpers.person_url(self, host: 'https://h4g3tf99-3000.use.devtunnels.ms')) #TODO: Change this to the domain
+      qrcode = RQRCode::QRCode.new(Rails.application.routes.url_helpers.person_url(self, host: 'https://ted-demo.onrender.com')) #TODO: Change this to the domain
       qrcode.as_svg(module_size: 6)
     end
   end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://h4g3tf99-5501.use.devtunnels.ms'  # TODO: Change this to the domain you will make the request fromtails
+      origins '*'  # TODO: Change this to the domain you will make the request fromtails
   
       resource '*',
         headers: :any,
