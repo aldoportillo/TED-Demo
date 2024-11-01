e1c178cf1e37cbf837892365f1de7e9d81a159cf
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-11-01 16:25:36 -0500

    Disable prod environment protection

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,6 @@ bundle install
 # For Ruby on Rails apps uncomment these lines to precompile assets and migrate your database.
  bundle exec rake assets:precompile
  bundle exec rake assets:clean
- bundle exec rake db:drop
+ DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
  bundle exec rake db:create
  bundle exec rake db:migrate
